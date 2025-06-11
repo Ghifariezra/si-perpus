bcd77908478bc2426f4b33501cb83660142731c8
Owner <ghifariezraramadhan@gmail.com>
2025-06-12 03:28:21 +0700

    test

--- main.cpp
+++ main.cpp
@@ -40,11 +40,5 @@ using namespace std;
 int main()
 {
     cout << "Hello, World!" << endl;
-    cout << "Hello, World!" << endl;
-    cout << "Hello, World!" << endl;
-    cout << "Hello, World!" << endl;
-    cout << "Hello, World!" << endl;
-    cout << "Hello, World!" << endl;
-    cout << "kkkk" << endl;
     return 0;
 } 
