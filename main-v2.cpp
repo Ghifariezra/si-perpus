d437d4d999a20d09236588375480df70717c0f26
Owner <ghifariezraramadhan@gmail.com>
2025-06-13 18:58:32 +0700

    update

---
+++ main-v2.cpp
@@ -0,0 +1,80 @@
+/*
+    Final Project - Sistem Informasi - Manajemen Perpustakaan
+    File Name: main.cpp
+
+    Kelompok:
+        •⁠ Fatimah (4524210039)
+        •⁠ Maghfiroh Lisabiliana (4524210040)
+        •⁠ Ghifari Ezra Ramadhan (4524210041)
+
+    Pembagian Materi:
+        Maghfiroh:
+            •⁠  ⁠Pointer
+            •⁠  ⁠⁠Stack
+        Fatimah:
+            •⁠  ⁠⁠Sorting
+            •⁠  ⁠⁠Queue
+        Fatimah & Maghfiroh:
+            •⁠  ⁠⁠Searching
+        Ghifari:
+            •⁠  ⁠⁠Tree
+            •⁠  ⁠⁠Graph
+
+    Materi:
+        •⁠  ⁠Pointer
+        •⁠  ⁠⁠Stack
+        •⁠  ⁠⁠Queue
+        •⁠  ⁠⁠Sorting
+        •⁠  ⁠⁠Searching
+        •⁠  ⁠⁠Tree
+        •⁠  ⁠⁠Graph
+*/
+
+/*
+    Final Project - Sistem Informasi - Manajemen Perpustakaan
+    File Name: main.cpp
+
+    Kelompok:
+        • Fatimah (4524210039)
+        • Maghfiroh Lisabiliana (4524210040)
+        • Ghifari Ezra Ramadhan (4524210041)
+
+    Pembagian Materi:
+        Maghfiroh:
+            • Pointer
+            • Stack
+        Fatimah:
+            • Sorting
+            • Queue
+        Fatimah & Maghfiroh:
+            • Searching
+        Ghifari:
+            • Tree
+            • Graph
+
+    Materi:
+        • Pointer
+        • Stack
+        • Queue
+        • Sorting
+        • Searching
+        • Tree
+        • Graph
+*/
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+void daftarBuku();
+void cariBuku();
+void pinjamBuku();
+void pengembalianBuku();
+void riwayatPeminjaman();
+void riwayatPengembalian();
+void rekomendasiBuku();
+
+int main() {
+    return 0;
+}
