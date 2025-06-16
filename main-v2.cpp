082e370a1c57bbcc8ae1df9523538b0769473ea4
Serendwin <twentyhours133@gmail.com>
2025-06-16 20:59:10 +0700

    tambah logic

--- main-v2.cpp
+++ main-v2.cpp
@@ -68,9 +68,101 @@
 
 using namespace std;
 
+// Queue dalam peminjaman buku
+typedef struct Node 
+{
+    string Fnama;
+    string FjudulBuku; 
+    Node* Fnext; 
+} Node;
+
+typedef struct Queue 
+{
+    Node* FFirst;
+    Node* FLast;
+} Queue;
+
+void initQueue(Queue &Q) 
+{
+    Q.FFirst = NULL;
+    Q.FLast = NULL;
+}
+
+void tambahPeminjam(Queue &Q, string Fnama, string FjudulBuku) 
+{
+    Node* Fbaru = new Node{Fnama, FjudulBuku, NULL};
+    if (Q.FFirst == NULL) 
+    {
+        Q.FFirst = Fbaru;
+        Q.FLast = Fbaru;
+    } 
+    else 
+    {
+        Q.FLast->Fnext = Fbaru;
+        Q.FLast = Fbaru;
+    }
+}
+
+void lihatAntrian(Queue Q) 
+{
+    Node* bantu = Q.FFirst;
+    if (bantu == NULL) 
+    {
+        cout << "\nAntrian kosong.\n";
+    } 
+    else 
+    {
+        cout << "\nDaftar Antrian Peminjam:\n";
+        while (bantu != NULL) 
+        {
+            cout << "- " << bantu->Fnama << " (Buku: " << bantu->FjudulBuku << ")\n";
+            bantu = bantu->Fnext;
+        }
+    }
+}
+ 
 void daftarBuku();
 void cariBuku();
-void pinjamBuku();
+
+void pinjamBuku()
+{
+    Queue Q;
+    initQueue(Q);
+
+    int pilihan;
+    string nama, buku;
+
+    do 
+    {
+        cout << "=== Sistem Peminjaman Buku (Queue) ===";
+        cout << "1. Tambah Peminjam\n";
+        cout << "2. Lihat Antrian\n";
+        cout << "3. Keluar\n";
+        cout << "Pilih opsi: ";
+        cin >> pilihan;
+        cin.ignore();
+
+        switch (pilihan) 
+        {
+            case 1:
+                cout << "Masukkan nama peminjam: ";
+                getline(cin, nama);
+                cout << "Masukkan judul buku yang dipinjam: ";
+                getline(cin, buku);
+                tambahPeminjam(Q, nama, buku);
+                break;
+            case 2:
+                lihatAntrian(Q);
+                break;
+            case 3:
+                cout << "Terima kasih telah menggunakan sistem ini.\n";
+                break;
+            default:
+                cout << "Opsi tidak valid. Silakan pilih ulang.\n";
+        }
+    } while (pilihan != 3); 
+}
+
 void pengembalianBuku();
 void rekomendasiBuku();
 void sortBuku();
